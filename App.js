3737d0c899f5c70d8c3909fabb566c25f3306daf
Lucasmdct <lucasmedeiroscosta07@gmail.com>
2023-07-18 19:00:53 -0300

    Aula react native app

--- App.js
+++ App.js
@@ -1,20 +1,57 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { StatusBar, Text, View, SafeAreaView, Image, ScrollView, Dimensions } from 'react-native';
+import { Alert, Button, TextInput, Appbar, Provider } from 'react-native-paper';
 
-export default function App() {
+const App = () => {
+  const [Input, setInput] = useState('');
+
+  const handleNomeChange = (text) => {
+    setInput(text);
+  };
+
+  const handleButtonPress = () => {
+    alert(`Olá, ${Input}!`);
+  };
+
+  // Obtém a largura total da tela
+  const screenWidth = Dimensions.get('window').width;
+
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+    <Provider>
+      <View>
+        <SafeAreaView>
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+          <Appbar.Header>
+            <Appbar.Content title="React-native" />
+          </Appbar.Header>
+          <StatusBar translucent={false} backgroundColor="#ff00ff" />
+          <Image
+            source={require('./imgs/react-app-banner.gif')}
+             style={{ flex: 1, width: screenWidth }}
+             resizeMode="contain"
+          />
+
+          <TextInput
+            placeholder='Informe seu nome'
+            style={{
+              fontSize: 32
+            }}
+            value={Input}
+            onChangeText={handleNomeChange}
+          />
+          <Button mode="contained" onPress={handleButtonPress}>
+            Obter Saudação
+          </Button>
+        </SafeAreaView>
+      </View>
+
+    </Provider>
+  );
+};
+
+export default App;
+
+
+
+
